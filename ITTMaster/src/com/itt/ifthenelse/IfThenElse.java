8983033f6a33e1836afcc1708d2c10bd58ec0e58
atyhu_000 <atyhu_000@Thats_funny.campus.ittesi.com>
2015-06-22 20:12:45 -0700

    my commit during class

---
+++ ITTMaster/src/com/itt/ifthenelse/IfThenElse.java
@@ -0,0 +1,16 @@
+package com.itt.ifthenelse;
+
+public class IfThenElse {
+	static int x = 30;
+
+	public static void main(String[] args) {
+		if (x < 20) {
+			System.out.println("yes x < 20");
+
+		} else {
+			System.out.println("no x is not less then 20");
+		}
+
+	}
+
+}
